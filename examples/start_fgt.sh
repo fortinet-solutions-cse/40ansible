8eea6b39bd41bc7472126349c21a54dc46480d9e
Miguel Angel Munoz <magonzalez@fortinet.com>
2018-02-08 22:03:50 +0100

    Minor bugs: Force static address and proper 'end'

--- examples/start_fgt.sh
+++ examples/start_fgt.sh
@@ -57,22 +57,24 @@ EOF
 
 cat >cfg-drv-fgt/openstack/latest/user_data <<EOF
 config system interface
-edit "port1"
-set ip 192.168.122.40/24
-set allowaccess https ping ssh snmp http telnet fgfm radius-acct probe-response capwap ftm
-next
+  edit "port1"
+    set mode static
+    set ip 192.168.122.40/24
+    set allowaccess https ping ssh snmp http telnet fgfm radius-acct probe-response capwap ftm
+  next
 end
 config system dns
-    set primary 8.8.8.8
-    set secondary 8.8.4.4
+  set primary 8.8.8.8
+  set secondary 8.8.4.4
 end
 config router static
-    edit 2
-        set gateway 192.168.122.1
-        set device "port1"
-    next
+  edit 2
+    set gateway 192.168.122.1
+    set device "port1"
+  next
+end
 EOF
 
 sudo mkisofs -publisher "OpenStack Nova 12.0.2" -J -R -V config-2 -o ${SF2_NAME}-cidata.iso cfg-drv-fgt
-virt-install --connect qemu:///system --noautoconsole --filesystem ${PWD},shared_dir --import --name ${SF2_NAME} --ram 1024 --vcpus 1 --disk fortios.qcow2,size=3 --disk fgt-logs.qcow2,size=30 --disk ${SF2_NAME}-cidata.iso,device=cdrom,bus=ide,format=raw,cache=none --network bridge=virbr0,mac=${SF2_MAC_ADMIN},model=virtio
+virt-install --connect qemu:///system --noautoconsole --filesystem ${PWD},shared_dir --import --name ${SF2_NAME} --ram 1024 --vcpus 1 --disk fortios.qcow2,size=3 --disk fgt-logs.qcow2,size=3 --disk ${SF2_NAME}-cidata.iso,device=cdrom,bus=ide,format=raw,cache=none --network bridge=virbr0,mac=${SF2_MAC_ADMIN},model=virtio
 
